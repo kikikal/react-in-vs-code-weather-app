221eddb16acfe7b102d5092d1914078f896d4c5c
kikikal <156766658+kikikal@users.noreply.github.com>
2024-05-14 15:07:02 +1000

    updated footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Weather from "./Weather"
+import Weather from "./Weather";
 
 function App() {
   return (
@@ -7,6 +7,24 @@ function App() {
       <header className="App-header">
         <h1>Weather Search Engine</h1>
         <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/kikikal/react-in-vs-code-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Christina Kalyvas
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://weather-search-engine-using-react-ck.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
       </header>
     </div>
   );
